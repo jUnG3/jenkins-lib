bcbe24722b0abdcccc85761af620b6c7d7c4dd47
Stjepan Soldo <soldo.stjepan1304@gmail.com>
2020-04-03 10:20:16 +0200

    Yolo

--- vars/choices.groovy
+++ vars/choices.groovy
@@ -2,5 +2,5 @@ import de.jenkins.util.ArtifactoryDockerRepository
 
 def List<String> getSome() {
     a = new ArtifactoryDockerRepository()
-    return a.dockerChoices(usernamePassword(credentialsId: 'test-cred', usernameVariable: 'username', passwordVariable: 'password'))
+    return a.dockerChoices(null)
 }
