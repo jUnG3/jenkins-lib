4e50cf9708650108acb0414ea0de8c45d9f97837
Stjepan Soldo <soldo.stjepan1304@gmail.com>
2020-04-03 09:25:32 +0200

    Brute force

--- src/de/jenkins/util/ArtifactoryDockerRepository.groovy
+++ src/de/jenkins/util/ArtifactoryDockerRepository.groovy
@@ -1,4 +1,12 @@
 package de.jenkins.util
 
+import org.jenkinsci.plugins.credentialsbinding.impl.UsernamePasswordMultiBinding
+
 class ArtifactoryDockerRepository {
+
+    private String baseUrl = "https://localhost"
+
+    public List<String> dockerChoices(UsernamePasswordMultiBinding auth) {
+        return new ArrayList<String>()
+    }
 }
